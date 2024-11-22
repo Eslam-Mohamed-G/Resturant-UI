20fa9e384a340d4a473008e35208a667dd5d328d
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2024-11-22 19:42:06 +0200

    React How to Calculate Product Price with Quantity and Options?

--- src/components/Price.tsx
+++ src/components/Price.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 
 type Props = {
     id:number;
@@ -8,29 +9,44 @@ type Props = {
 }
 
 const Price = ({price, id, options}:Props) => {
+    const [total, setTotal] = useState(price)
+    const [quantity, setQuantity] = useState(1)
+    const [selected, setSelected] = useState(0)
   return (
-    <div className='w-full flex flex-col gap-8'>
-        <h2 className='text-2xl font-bold'>${price.toFixed(2)}</h2>
-        {/* option */}
-        <div className='flex flex-row items-center justify-center gap-4'>
-            {options?.map(option=>(
-                <button key={option.title} className='border border-red-300 py-1 px-5 md:px-8 rounded hover:bg-red-400 hover:text-white'>{option.title}</button>
-            ))}
+    <div className="w-full flex flex-col gap-8">
+      <h2 className="text-2xl font-bold">${total.toFixed(2)}</h2>
+      {/* option */}
+      <div className="flex flex-row items-center justify-center gap-4">
+        {options?.map((option, index) => (
+          <button
+            key={option.title}
+            className="border border-red-300 py-1 px-5 md:px-8 rounded hover:bg-red-400 hover:text-white"
+            style={{
+                background: selected===index? "rgb(248 113 113)" : "white",
+                color: selected===index ? "white" : "red"
+            }}
+            onClick={()=>setSelected(index)}
+          >
+            {option.title}
+          </button>
+        ))}
+      </div>
+      {/* quantity */}
+      <div className="flex justify-between w-full border">
+        <div className="flex flex-1 items-center justify-between border border-red-300 px-1 md:px-3">
+          <span>Quantity</span>
+          <div className="flex gap-2 items-center justify-center">
+            <button onClick={()=>setQuantity(prv=> prv > 1 ? prv - 1 : 1)}>{"<"}</button>
+            <span>{quantity}</span>
+            <button onClick={()=>setQuantity(prv=> prv +1)}>{">"}</button>
+          </div>
         </div>
-        {/* quantity */}
-        <div className='flex justify-between w-full border'>
-            <div className='flex flex-1 items-center justify-between border border-red-300 px-1 md:px-3'>
-                <span>Quantity</span>
-                <div className='flex gap-2 items-center justify-center'>
-                    <button>{'<'}</button>
-                    <span>1</span>
-                    <button>{'>'}</button>
-                </div>
-            </div>
-            <button className='bg-red-500 text-white uppercase py-2 px-6'>add to cart</button>
-        </div>
+        <button className="bg-red-500 text-white uppercase py-2 px-6">
+          add to cart
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Price
