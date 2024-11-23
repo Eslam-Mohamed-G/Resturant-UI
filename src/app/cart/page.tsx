9f50222e6f2e839ae4589d821dd6e470deeb2027
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2024-11-23 11:39:16 +0200

    Tailwind Cart Page Design

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -3,28 +3,43 @@ import React from 'react'
 
 const CartPage = () => {
   return (
-    <div className=''>
+    <div className='h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col lg:flex-row text-red-500'>
       {/* products */}
-      <div className=''>
+      <div className='"h-1/2 p-4 flex flex-col justify-center lg:h-full lg:w-2/3 2xl:w-1/2 lg:px-20 xl:px-40"'>
       {/* single item */}
-        <div className=''>
+        <div className=' flex items-center justify-between mb-4'>
           <Image src={''} alt={''}/>
           <div className=''>
-            <h1>sicilian</h1>
+            <h1 className='uppercase text-xl font-bold'>sicilian</h1>
             <span>large</span>
           </div>
-          <h2>$7</h2>
-          <span>x</span>
+          <h2 className='font-bold'>$7</h2>
+          <span className='cursor-pointer'>x</span>
         </div>
       </div>
       {/* payment */}
-      <div className=''>
-        <div className=''>
+      <div className="h-1/2 p-4 bg-fuchsia-50 flex flex-col gap-4 justify-center lg:h-full lg:w-1/3 2xl:w-1/2 lg:px-20 xl:px-40 2xl:text-xl 2xl:gap-6">
+        <div className='flex justify-between'>
           <span className=''>subtotal</span>
           <span className=''>$8</span>
         </div>
-        <hr className=''/>
-        <button className='bg-red-500 text-white p-3 rounded-md uppercase'>checkout</button>
+
+        <div className="flex justify-between">
+          <span className="">Service Cost</span>
+          <span className="">$0.00</span>
+        </div>
+
+        <div className="flex justify-between">
+          <span className="">Delivery Cost</span>
+          <span className="text-green-500">FREE!</span>
+        </div>
+
+        <hr className="my-2" />
+        <div className="flex justify-between">
+          <span className="">TOTAL(INCL. VAT)</span>
+          <span className="font-bold">$81.70</span>
+        </div>
+        <button className="bg-red-500 text-white p-3 rounded-md w-1/2 self-end uppercase">checkout</button>
       </div>
     </div>
   )
