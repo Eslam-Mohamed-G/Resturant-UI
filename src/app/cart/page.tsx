e7aa3dbdbb60adde52f6b95672ba0b87f200b56e
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2024-11-23 11:18:41 +0200

    Tailwind Cart Page Design

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,8 +1,32 @@
+import Image from 'next/image'
 import React from 'react'
 
 const CartPage = () => {
   return (
-    <div>CartPage</div>
+    <div className=''>
+      {/* products */}
+      <div className=''>
+      {/* single item */}
+        <div className=''>
+          <Image src={''} alt={''}/>
+          <div className=''>
+            <h1>sicilian</h1>
+            <span>large</span>
+          </div>
+          <h2>$7</h2>
+          <span>x</span>
+        </div>
+      </div>
+      {/* payment */}
+      <div className=''>
+        <div className=''>
+          <span className=''>subtotal</span>
+          <span className=''>$8</span>
+        </div>
+        <hr className=''/>
+        <button className='bg-red-500 text-white p-3 rounded-md uppercase'>checkout</button>
+      </div>
+    </div>
   )
 }
 
